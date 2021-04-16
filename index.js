dd0aaecc628ae87b24dc7bbce1edc4e56147a6eb
Elisa Tumwebaze <tumwebazeelisa@gmail.com>
2021-04-16 18:14:55 +0300

    changed the name of the route

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.listen(3000, 'localhost',()=>{
     console.log('server created');
 
 });
-app.get('/',(req,res)=>{
+app.get('/todos',(req,res)=>{
 const todos=['waking up','bathing','brushing'];
 res.send(todos);
 })
